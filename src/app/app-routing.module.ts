f52066218c34a53edfb31ae477ede891265c77e4
Dominik Strnad <99843403+dominstr@users.noreply.github.com>
2025-01-10 16:09:09 +0100

    Remove default pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,21 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'favourites',
+    loadChildren: () => import('./favourites/favourites.module').then( m => m.FavouritesPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+
+
 ];
 @NgModule({
   imports: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: 'fadb0eefc3eee853077299ad4d5e62d7',
+  API_URL: 'https://api.openweathermap.org/data/2.5/',
 };
 
 /*
